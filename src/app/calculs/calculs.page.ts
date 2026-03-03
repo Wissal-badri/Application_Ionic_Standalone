92937b93deba09254bcb4662094ae22e5bd59452
Wissal-badri <badriwiwi1@gmail.com>
2026-03-03 14:54:33 +0000

    feat: implement a new calculator page with basic arithmetic operations and UI.

--- src/app/calculs/calculs.page.ts
+++ src/app/calculs/calculs.page.ts
@@ -11,10 +11,40 @@ import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonButton, IonCa
   imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonLabel, IonButton, IonCardTitle, IonCard, IonCardHeader, IonCardContent, IonInput, IonItem, IonList, IonCardSubtitle]
 })
 export class CalculsPage implements OnInit {
+  num1?: number;
+  num2?: number;
+  resultat: number | string = 'RÉSULTAT';
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  effacer() {
+    this.num1 = undefined;
+    this.num2 = undefined;
+    this.resultat = 'RÉSULTAT';
+  }
+
+  calculer(operation: string) {
+    if (this.num1 === undefined || this.num2 === undefined) {
+      this.resultat = 'Entrez des nombres';
+      return;
+    }
+    switch (operation) {
+      case '+':
+        this.resultat = this.num1 + this.num2;
+        break;
+      case '-':
+        this.resultat = this.num1 - this.num2;
+        break;
+      case '*':
+        this.resultat = this.num1 * this.num2;
+        break;
+      case '/':
+        this.resultat = this.num2 !== 0 ? Number((this.num1 / this.num2).toFixed(2)) : 'Erreur (Div/0)';
+        break;
+    }
+  }
 }
+
